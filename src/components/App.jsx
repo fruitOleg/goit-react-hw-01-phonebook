9bcf6782b0c041787657c10d6c6e6fdc8f10678f
not oleg <104638867+fruitOleg@users.noreply.github.com>
2023-08-13 17:21:59 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from "react";
+import { GlobalStyle } from "./GlobalStyle";
+import { Section } from "./Section/Section";
+
+export class App extends Component {
+   state = {
+  contacts: [],
+  name: ''
+   } 
+   render() {
+      return (
+         <div>
+            <GlobalStyle />
+            
+
+
+            <Section title = 'Phonebook'></Section>
+      </div>)
+      
+   }
 };
